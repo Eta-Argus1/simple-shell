03ae97ccd70b450e1ff9b7e542ce68264cdb7ed9
Eta-Argus1 <bourkali-ayoub@outlook.com>
2023-10-18 03:31:05 -0700

    message

--- h_signal.c
+++ h_signal.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * ctrl_c - ignore Ctrl-C input and prints prompt again
+ * handle_ctrlc - ignore Ctrl-C input and prints prompt again
  * @n: takes in int from signal
  */
 void handle_ctrlc(int n)
 {
-        (void)n;
+	(void)n;
 	write(STDOUT_FILENO, "\n$ ", 3);
 }
